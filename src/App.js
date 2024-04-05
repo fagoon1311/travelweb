ddc84d24b837055fed669fd59aecf1d62697ec45
fagoon1311 <fs14smailbox65@gmail.com>
2024-04-05 14:04:20 +0530

    Setup- Routes, CSS and components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './pages/Layout';
+import About from './pages/About';
+import Blogs from './pages/Blogs';
+import BlogsDetails from './pages/BlogsDetails'
+import Home from './pages/Home'
+import NoPage from './pages/NoPage'
+import PlacesRoutes from './pages/PlacesRoutes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} /> 
+            <Route path='/about' element={<About />} />
+            <Route path='/blogs' element={<Blogs />} />
+            <Route path='/blogs/:id' element={<BlogsDetails />} />
+            <Route path='/places' element={<PlacesRoutes />} /> 
+            <Route path='*' element={<NoPage />} />       
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
